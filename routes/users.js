f7e146ab49cbf2491af7e6896edd9324738a66a8
zufrizalyordan <zufrizalyordan@gmail.com>
2014-04-11 18:46:35 +0700

    processing the add user

--- routes/users.js
+++ routes/users.js
@@ -22,4 +22,32 @@ router.get('/new', function(req, res) {
   res.render('useradd', { title: 'Add New User' });
 });
 
+router.post('/send', function(req, res) {
+  return function(req, res) {
+    // Get our form values. These rely on the "name" attributes
+    var userName = req.body.username;
+    var userEmail = req.body.useremail;
+
+    // Set our collection
+    var collection = db.get('usercollection');
+
+    // Submit to the DB
+    collection.insert({
+      "username" : userName,
+      "email" : userEmail
+    }, function (err, doc) {
+      if (err) {
+        // If it failed, return error
+        res.send("There was a problem adding the information to the database.");
+      }
+      else {
+        // If it worked, set the header so the address bar doesn't still say /useradd
+        res.location("userlist");
+        // And forward to success page
+        res.redirect("/");
+      }
+    });
+  }
+});
+
 module.exports = router;
